80fd880ed292dec6c784b67f58dbe3a6537caa11
alien087 <44940977+alien087@users.noreply.github.com>
2018-12-12 15:06:54 +0700

    Update mastertubes.cpp

--- mastertubes.cpp
+++ mastertubes.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 #include<conio.h>
 #include<cstdlib>
+#include<windows.h>
 using namespace std;
 
 #define REPup(i, a, p)  for(int (i) = int (a); i<int (p); i++)
 #define REPdown(i, a, p)  for(int (i) = int (a); i>=int (p); i--)
-
 typedef char infotype;
 typedef struct TElmtList *address;
 typedef struct TElmtList{
@@ -30,6 +30,7 @@ typedef struct {
 #define Nil NULL
 #define Infinity 99999
 
+void pilihMenuu(List *data, List data2, int *nyawa);
 
 /****************** TEST LIST KOSONG ******************/
 bool IsEmpty(List L) {
@@ -61,10 +62,11 @@ void Deallocation(address hapus) {
 
 
 /****************** PENCARIAN SEBUAH ELEMEN LIST ******************/
+
 address Search1(List L, infotype X, infotype y){
 	address P = First(L);
 	while (P != Nil){
-		if ( Prev(P) != Nil && (Next(P)->info == y && Prev(P)->info == X) && Info(P) == '_' ){
+		if ( Prev(P) != Nil && Next(P) != Nil && (Next(P)->info == y && Prev(P)->info == X) && Info(P) == '_' ){
 			return P;
 		}
 		P = Next(P);
@@ -94,137 +96,16 @@ void InsertLast(List *L, infotype x) {
  address NewElmt;
  NewElmt = Allocation(x);
  if (NewElmt != NULL) {
- if(IsEmpty(*L)){
- InsertFirst(L,x);
- } else{
- Next(Last(*L)) = NewElmt;
- Prev(NewElmt) = Last(*L);
- Last(*L) = NewElmt;
- }
- }
-}
-
-
-/****************** PRIMITIF BERDASARKAN ALAMAT ******************/
-/*** PENAMBAHAN ELEMEN BERDASARKAN ALAMAT ***/
-void InsertFirst (List *L, address P){
-/* I.S. Sembarang, P sudah dialokasi */
-/* F.S. Menambahkan elemen ber-address P sebagai elemen pertama */
 	if(IsEmpty(*L)){
- First(*L) = P;
- Last(*L) = P;
- } else{
- Next(P) = First(*L);
- Prev(First(*L)) = P;
- First(*L) = P;
- }
+		InsertFirst(L,x);
+	 } else{
+		Next(Last(*L)) = NewElmt;
+		Prev(NewElmt) = Last(*L);
+		Last(*L) = NewElmt;
+ 	}
+}
 }
 
-void InsertLast (List *L, address P){
-/* I.S. Sembarang, P sudah dialokasi */
-/* F.S. P ditambahkan sebagai elemen terakhir yang baru */
-	if(IsEmpty(*L)){
- InsertFirst(L,P);
- } else{
- Next(Last(*L)) = P;
- Prev(P) = Last(*L);
- Last(*L) = P;
- }
-}
-
-void InsertAfter (List *L, address P, address Prec){
-
-	if(Next(Prec) == Nil){
-		InsertLast(L,P);
-	}
-	else{
-		Next(P) = Next(Prec);
-		Prev(P) = Prec;
-		Prev(Next(Prec))= P;
-		Next(Prec) = P;
-	}
-}
-
-void InsertBefore (List *L, address P, address Succ){
-	if(Succ == First(*L)){
-	InsertFirst(L,P);
-	}else{
-	Next(Prev(Succ)) = P;
-	Prev(P) = Prev(Succ);
-	Next(P) = Succ;
-	Prev(Succ) = P;
-	}
-}
-
-
-/*** PENGHAPUSAN SEBUAH ELEMEN ***/
-
-void DelFirst (List *L, address *P){
-	*P = First(*L);
-	First(*L) = Next(*P);
-	Prev(First(*L)) = Nil;
-	Deallocation(*P);
-}
-
-void DelLast (List *L, address *P){
-	*P = Last(*L);
-	Last(*L) = Prev(*P);
-	Next(Last(*L)) = Nil;
-	Prev(*P) = Nil;
-	Deallocation(*P);
-}
-
-
-
-void DelAfter (List *L, address *Pdel, address Prec){
-	*Pdel = Next(Prec);
-	Next(Prec) = Next(*Pdel);
-	Prev(Next(*Pdel)) = Prec;
-	Prev(*Pdel) = Nil;
-	Next(*Pdel)= Nil;
-	Deallocation(*Pdel);
-}
-
-void DelBefore (List *L, address *Pdel, address Succ){
-	*Pdel = Prev(Succ);
-	Next(Prev(*Pdel)) = Succ;
-	Prev(Succ) = Prev(*Pdel);
-	Prev(*Pdel) = Nil;
-	Next(*Pdel) = Nil;
-	Deallocation(*Pdel);
-}
-
-void DelP (List *L, infotype X){
-
-	address P = Searchh(*L,X);
-	address prec = Prev(P);
-	if (P != Nil){
-		if(P == First(*L)){
-			DelFirst(L,&P);
-		}else if(P == Last(*L)){
-			DelLast(L,&P);
-		}else{
-			DelAfter(L,&P,prec);
-		}
-	}
-}
-
-
-/****************** PROSES SEMUA ELEMEN LIST ******************/
-
-
-void PrintBackward (List L){
-	cout<<"[";
-	if(!IsEmpty(L)){
-		address temp =Last(L);
-		while(Prev(temp)!=NULL){
-			cout<<Info(temp) <<", ";
-			temp = Prev(temp);
-		}
-		cout<<Info(temp);
-	}
-	cout<<"]"<<endl;
-}
 
 void Outputdata(List L){
     address temp = (L).first;
@@ -235,25 +116,9 @@ void Outputdata(List L){
 
 }
 
-void cobaisi(List *data){
-	address temp;
-	temp = (*data).first;
-	temp->info = '9';
-	(temp->next)->info = '2';
-	REPup(i, 0, 5){
-		temp = temp->next;
-	}
-	temp->info = '1';
-	(temp->next)->info = '1';
 
-	REPup(i, 0, 3){
-		temp = temp->next;
-	}
 
-	temp->info = '2';
-}
-
-void interface(){
+void iinterface(){
 	cout<<"Posisi Tebakkan Anda: \n";
 	cout<<"a. Sebelum \nb. Setelah \nc. Awal \nd. Akhir\n";
 	cout<<"Masukkan Posisi: ";
@@ -269,41 +134,129 @@ bool cekNext(List L, address temp){
 }
 
 bool cekBetween(List L, address temp){
-	return (Prev(temp)!=NULL && Next(temp)!=NULL && Info(temp) == Info(Next(temp)) && Info(temp) == Info(Prev(temp)) && Next(temp) != NULL);
+	return (Prev(temp)!=NULL && Next(temp)!=NULL && Info(temp) == Info(Next(temp)) && Info(temp) == Info(Prev(temp)) );
+}
+
+void pushangka(int x, List *L){
+        if(x == 1){
+            InsertLast(&(*L), '1');
+        }else if( x == 2){
+            InsertLast(&(*L), '2');
+        }else if( x == 3){
+            InsertLast(&(*L), '3');
+        }else if( x == 4){
+            InsertLast(&(*L), '4');
+        }else if( x == 5){
+            InsertLast(&(*L), '5');
+        }else if( x == 6){
+            InsertLast(&(*L), '6');
+        }else if( x == 7){
+            InsertLast(&(*L), '7');
+        }else if( x == 8){
+            InsertLast(&(*L), '8');
+        }else if( x == 9){
+            InsertLast(&(*L), '9');
+        }else{
+            InsertLast(&(*L), '0');
+        }
+
+}
+
+void randomLIST(List *L, int i){
+    int x,y,z = 0,z2 = 0;
+	if(i == 0){
+		srand(time(0));
+	}
+    REPup(j, 0, 10){
+        x = rand()%10;
+        y = rand()%2;
+
+        if(y == 1){
+          if(z < 5)pushangka(x, &(*L));
+          else InsertLast(&(*L), '_');
+          z++;
+        }else{
+          if(z2 > 4)pushangka(x, &(*L));
+          else InsertLast(&(*L), '_');
+          z2++;
+        }
+    }
+
+}
+
+void interface1(){
+	cout<<"Selama Datang Di Game Zuma\n";
+	cout<<"1. Pilih soal\n2. Tentukan Kesempatan Menembak\n3. Mulai Game\n4. Keluar\n";
+	cout<<"Masukkan Pilihan Anda: ";
+}
+
+void pilihSoal(List *data, List data2, int *nyawa){
+	system("cls");
+	cout<<"Pilihan Soal: \n";
+	char pilih;
+	cout<<"a. "; Outputdata(*data); cout<<"\n";
+	cout<<"b. "; Outputdata(data2); cout<<"\n";
+	cout<<"Masukkan Pilihan Soal: "; cin>>pilih;
+	if(pilih == 'b') *data=data2;
+	pilihMenuu(data, data2, nyawa);
+
+}
+void pilihMenuu(List *data, List data2, int *nyawa){
+	system("cls");
+	interface1();
+	char pilih;
+	cin>>pilih;
+	switch (pilih){
+		case '1' : pilihSoal(data, data2, nyawa); break;
+		case '2' : system("cls"); cout<<"Masukkan Nyawa : "; cin>>*nyawa; pilihMenuu(data, data2, nyawa); break;
+		case '3' : break;
+		case '4' : exit(0); break;
+		default  : cout<<"Masukkan Salah!, Silahkan Masukkan Pilihan Lagi: "; pilihMenuu(data, data2, nyawa); 
+	}
+	system("cls");
 }
 
 
 int main(){
 	address temp;
-	List data;
-	int nyawa=4; // Variabel banyaknya kesempatan USER untuk menembak angka
+	List data, data2;
+	int nyawa; // Variabel banyaknya kesempatan USER untuk menembak angka
 	char pilih;
 	infotype x, n, y;
-	
 	createList(&data); //Membuat List Kosong
+	createList(&data2);
 	
-	REPup(i, 0, 10){
-        InsertLast(&data, '_' ); // Memasukkan "_" kedalam List
-    }
+	randomLIST(&data,0);
+	// Sleep(100);
+	randomLIST(&data2,1);
 
-	cobaisi(&data); // Mengisi angka Random Kedalam List
+	pilihMenuu(&data, data2, &nyawa);
+	
+	
+
+	//cobaisi(&data); // Mengisi angka Random Kedalam List
 	while(nyawa>0){
+		
+		system("cls");
+		// play:
 		Outputdata(data); // Menampilkan Susunan Angka
 		cout<<"\nKesempatan Menembak: "<<nyawa; // Menampilkan sisa Kesempatan Menembak
 		cout<<"\nMasukkan Angka Tebakan anda: "; cin>>n; // Meminta user menginputkan Angka yang akan ditembak
-		interface(); // Memanggil fungsi Interface
 		
+		iinterface(); // Memanggil fungsi Interface
+		
+		play:
 		cin>>pilih; // User Memasukkan Pilihan Posisi
 		switch(pilih){ // Case untuk Pilihan Posisi
 			case 'a' :	cout<<"Angka: "; cin>>x; // Meminta user memasukkan Posisi Sebelum
 						cout<<"Setelah Angka: "; cin>>y; //Meminta user memasukkan Posisi Setelah
 						temp = Search1(data, y, x); // Proses pencarian posisi
-						Info(temp) = n; // Proses reWrite/replace Info dengan angka pilihan user
+						if(temp!=Nil) Info(temp) = n; // Proses reWrite/replace Info dengan angka pilihan user
 						break;
 			case 'b' :	cout<<"Angka: "; cin>>x; // Meminta user memasukkan Posisi Setelah
 						cout<<"Sebelum Angka: "; cin>>y; // Meminta user memasukkan Posisi Sebelum
 						temp = Search1(data, x, y); // Proses pencarian posisi
-						Info(temp) = n; // Proses reWrite/replace Info dengan angka pilihan user
+						if(temp!=Nil) Info(temp) = n; // Proses reWrite/replace Info dengan angka pilihan user
 						break;
  			case 'c' :	First(data)->info = n; // Proses reWrite/replace Info elemen pertama dengan angka pilihan user
 						temp = First(data);
@@ -311,10 +264,12 @@ int main(){
 			case 'd' : 	Last(data)->info = n; // Proses reWrite/replace Info elemen terakhir dengan angka pilihan user
 						temp = Last(data);
 						break;
+			default  : cout<<"Masukkan salah! Silahkan masukkan Lagi: "; goto play; break;
 		}
 		
 		Outputdata(data);
-		if(cekPrev(data, temp) || cekNext(data, temp) || cekBetween(data, temp)){ // Pengecekan kondisi apakah sudah berjejer 3 atau lebih angka yang sama
+
+		if(temp != Nil && (cekPrev(data, temp) || cekNext(data, temp) || cekBetween(data, temp))){ // Pengecekan kondisi apakah sudah berjejer 3 atau lebih angka yang sama
 			cout<<"\nAnda Beruntung"; // Jika sudah makan akan output "Anda Beruntung" dan
 			break; 					  // proses perulangan dihentikan
 		}
@@ -323,8 +278,11 @@ int main(){
 
 	}
 
-	if(nyawa<1) cout<<"Anda Kurang Beruntung"; // Jika kesempatan menembak habis (daalam hal ini <1) maka
+	if(nyawa<1) {
+		Outputdata(data); cout<<endl;
+		cout<<"Anda Kurang Beruntung"; // Jika kesempatan menembak habis (daalam hal ini <1) maka
     											   // Output "anda Kurang Beruntung"
+	}
 	
 	getche();
 
